a3b8e4e9d1909928d9f8556fa031b66d711d9b26
injuly <srijannr1@gmail.com>
2023-12-07 21:21:13 +0530

    refactor

--- app/Solution/Day7.hs
+++ app/Solution/Day7.hs
@@ -1,14 +1,14 @@
 module Solution.Day7 (part1, part2) where
 
-import Data.Functor ((<&>))
 import Data.Bifunctor (Bifunctor (bimap))
-import Data.List (elemIndex, sortBy, maximumBy, sort)
-import Data.List.Utils (uniq)
-import Debug.Trace (trace)
-import Util (count, orElse, readInt, (|>))
 import Data.Function (on)
+import Data.Functor ((<&>))
+import Data.List (elemIndex, maximumBy, sort, sortBy, sortOn)
+import Data.List.Utils (uniq)
+import Data.Ord (Down (Down))
+import Util (count, orElse, readInt, (|>))
 
-data Power
+data HandType
   = HighCard
   | OnePair
   | TwoPairs
@@ -17,14 +17,14 @@ data Power
   | FourOfKind
   | FiveOfKind
   deriving (Show, Eq, Ord)
-data Hand = Hand String Power deriving (Show)
-type Bids = [(Hand, Int)] -- (Hand, Bid amount)
 
-getUniqueCharCounts :: String -> [(Char, Int)]
-getUniqueCharCounts s =  uniq s <&> (\c -> (c, count c s))
+data Hand = Hand String HandType deriving (Show)
 
-getPower :: [Int] -> Power
-getPower counts = case counts of
+uniqueFrequencies :: String -> [Int]
+uniqueFrequencies s = (`count` s) <$> uniq s
+
+getHandType :: [Int] -> HandType
+getHandType counts = case counts of -- `counts` must be sorted in asc order
   [5] -> FiveOfKind
   [1, 4] -> FourOfKind
   [2, 3] -> FullHouse
@@ -33,31 +33,28 @@ getPower counts = case counts of
   [1, 1, 1, 2] -> OnePair
   other -> HighCard
 
-powerOfCard :: String -> Power
-powerOfCard = getPower . sort . map snd . getUniqueCharCounts 
-
-powerOfCard2 :: String -> Power
+powerOfCard, powerOfCard2 :: String -> HandType
+powerOfCard = getHandType . sort . uniqueFrequencies
 powerOfCard2 "JJJJJ" = FiveOfKind
-powerOfCard2 s = 
+powerOfCard2 s =
   let s' = filter (/= 'J') s
       numJokers = 5 - length s'
-      countOfChars = getUniqueCharCounts s'
-      maxCount:restCounts = snd <$> sortBy (flip compare `on` snd) countOfChars
-      sortedCounts = (maxCount + numJokers) : restCounts 
-  in getPower $ reverse sortedCounts 
+      countOfChars = uniqueFrequencies s'
+      maxCount : restCounts = sortOn Down countOfChars
+      sortedCounts = reverse $ (maxCount + numJokers) : restCounts
+   in getHandType sortedCounts
 
--- A has highest power => highest index. hence the reverse.
-cards = reverse "AKQJT98765432"
+cards = reverse "AKQJT98765432" -- A has highest power => highest index. hence the reverse.
 cards2 = reverse "AKQT98765432J"
 
 compareHandsWith :: [Char] -> Hand -> Hand -> Ordering
-compareHandsWith cardList (Hand s1 p1) (Hand s2 p2) = case compare p1 p2 of
-  EQ -> compareS s1 s2
-  ltOrGt -> ltOrGt
+compareHandsWith cardList (Hand s1 type1) (Hand s2 type2) =
+	case compare type1 type2 of
+    EQ -> compareS s1 s2
+    ltOrGt -> ltOrGt
   where
-    cardStr c = elemIndex c cardList `orElse` error ("invalid card: " <> [c])
-    compareS :: String -> String -> Ordering
-    compareS (a : as) (b : bs) = case compare (cardStr a) (cardStr b) of
+    cardStrength c = elemIndex c cardList `orElse` error ("invalid card: " <> [c])
+    compareS (a : as) (b : bs) = case (compare `on` cardStrength) a b of
       EQ -> compareS as bs
       ltOrGt -> ltOrGt
 
@@ -68,13 +65,16 @@ parseWith powerFn = map (bimap parseHand readInt . span (/= ' ')) . lines
   where
     parseHand s = Hand s (powerFn s)
 
-solveWith :: (String -> Power) -> String  -> String -> Int
+solveWith :: (String -> HandType) -> String -> String -> Int
 solveWith powerFn cardList s = snd $ foldl combine (1, 0) sortedCards
   where
-    sortedCards = s |> parseWith powerFn |> sortBy (compareBidsWith cardList) |> map snd
+    sortedCards =
+      s
+        |> parseWith powerFn
+        |> sortBy (compareBidsWith cardList)
+        |> map snd
     combine (i, acc) x = (i + 1, x * i + acc)
 
-part1, part2 :: String -> Int
-part1 = solveWith powerOfCard cards 
-part2 = solveWith powerOfCard2 cards2
+part1 = solveWith powerOfCard cards
 
+part2 = solveWith powerOfCard2 cards2
