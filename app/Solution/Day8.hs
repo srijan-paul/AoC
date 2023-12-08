285585e146f83d13841ee39aadd92f72c51df2f4
injuly <srijannr1@gmail.com>
2023-12-08 22:36:58 +0530

    format

--- app/Solution/Day8.hs
+++ app/Solution/Day8.hs
@@ -4,30 +4,29 @@ import Control.Arrow ((&&&))
 import Data.Bifunctor (Bifunctor (bimap))
 import Data.Char (isAlpha, isDigit, isSpace)
 import Data.List.Extra (split, splitOn, stripPrefix, stripSuffix, trim, wordsBy)
-import qualified Data.Map as M ((!), Map, fromList)
-import Data.Tuple.Extra (both, second, first)
-import Util (first2, stripPrefix', stripSuffix')
+import Data.Map qualified as M (Map, fromList, (!))
+import Data.Tuple.Extra (both, first, second)
 import Debug.Trace (trace)
+import Util (first2, stripPrefix', stripSuffix')
 
 type Map = M.Map String (String, String)
+
 type Network = (String, Map)
 
 parse = bimap cycle parseMap . (head &&& last) . splitOn "\n\n"
   where
-    parseMap = M.fromList . map (toTup  . wordsBy (`elem` "()=, ")) . lines
-    toTup (a:b:c:_) = (a, (b, c))
-
+    parseMap = M.fromList . map (toTup . wordsBy (`elem` "()=, ")) . lines
+    toTup (a : b : c : _) = (a, (b, c))
 
 solve :: (String, Map) -> Int
 solve (cs, m) = go (m M.! "AAA") cs 1
   where
-    go pair (x:xs) i = case select x pair of
+    go pair (x : xs) i = case select x pair of
       "ZZZ" -> i
-      next  ->  go (m M.! next) xs (i + 1)
+      next -> go (m M.! next) xs (i + 1)
 
     select 'L' = fst
     select _ = snd
-
 
 solve2 :: (String, Map) -> Int
 solve2 = undefined
